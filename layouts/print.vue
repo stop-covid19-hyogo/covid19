41a0182b4c30380757715d5277aa4ddfba705413
saigaso <saigaso1238@gmail.com>
2020-03-14 15:59:18 +0900

    リンクを兵庫版に修正しました

--- layouts/print.vue
+++ layouts/print.vue
@@ -32,7 +32,7 @@
                 ※最新の情報はWebページをご覧ください
               </p>
               <p class="PrintMeta-Link">
-                https://stopcovid19.metro.tokyo.lg.jp/
+                https://stop-covid19-hyogo.org/
               </p>
             </v-card>
           </v-card>
@@ -77,7 +77,7 @@ export default {
       link: [
         {
           rel: 'canonical',
-          href: `https://stopcovid19.metro.tokyo.lg.jp${this.$route.path}`
+          href: `https://stop-covid19-hyogo.org${this.$route.path}`
         }
       ]
     }
