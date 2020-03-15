670c8f90037a40bbe64ac1081be66b13dc297eb9
saigaso <saigaso1238@gmail.com>
2020-03-15 16:54:53 +0900

    サイトタイトルを修正しました

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ const config: Configuration = {
     htmlAttrs: {
       prefix: 'og: http://ogp.me/ns#'
     },
-    titleTemplate: '%s | 兵庫県非公式 新型コロナウイルス感染症まとめサイト',
+    titleTemplate: '%s | 兵庫県非公式 新型コロナウイルスまとめサイト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -24,7 +24,7 @@ const config: Configuration = {
       {
         hid: 'og:site_name',
         property: 'og:site_name',
-        content: '兵庫県非公式 新型コロナウイルス感染症まとめサイト'
+        content: '兵庫県非公式 新型コロナウイルスまとめサイト'
       },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       {
@@ -35,7 +35,7 @@ const config: Configuration = {
       {
         hid: 'og:title',
         property: 'og:title',
-        content: '兵庫県 新型コロナウイルス感染症まとめサイト'
+        content: '兵庫県非公式 新型コロナウイルスまとめサイト'
       },
       {
         hid: 'og:description',
@@ -168,7 +168,7 @@ const config: Configuration = {
     hardSource: process.env.NODE_ENV === 'development'
   },
   manifest: {
-    name: '兵庫県 新型コロナウイルス感染症まとめサイト',
+    name: '兵庫県非公式 新型コロナウイルスまとめサイト',
     theme_color: '#01a0c7',
     background_color: '#ffffff',
     display: 'standalone',
