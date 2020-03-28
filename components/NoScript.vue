225b7a8556a35aa75f771668c86e9efb20f0776f
y-chan <y_chan_crypto@yahoo.co.jp>
2020-03-29 00:58:52 +0900

    change tokyo to hyogo

--- components/NoScript.vue
+++ components/NoScript.vue
@@ -11,8 +11,8 @@
         }
       </style>
       <div class="noscript-heading">
-        <img src="/logo.svg" :alt="$t('東京都')" />
-        {{ $t('新型コロナウイルス感染症') }}<br />{{ $t('対策サイト') }}
+        <img src="/logo.svg" :alt="$t('兵庫県')" />
+        {{ $t('新型コロナウイルス') }}<br />{{ $t('まとめサイト') }}
       </div>
       <div class="noscript-body">
         {{ $t('当サイトではJavaScriptを使用しております。') }}<br />
