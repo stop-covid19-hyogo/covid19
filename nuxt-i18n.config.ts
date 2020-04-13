639f9245b6f191bc1f5945ce1fe2ce297f304518
y-chan <y_chan_crypto@yahoo.co.jp>
2020-04-13 17:03:59 +0900

    disabled other language

--- nuxt-i18n.config.ts
+++ nuxt-i18n.config.ts
@@ -21,7 +21,7 @@ const options: NuxtVueI18n.Options.AllOptionsInterface = {
       iso: 'ja-JP',
       file: 'ja.json',
       description: 'Japanese'
-    },
+    } /* ,
     {
       code: 'en',
       name: 'English',
@@ -65,7 +65,7 @@ const options: NuxtVueI18n.Options.AllOptionsInterface = {
       iso: 'ja-JP',
       file: 'ja-Hira.json',
       description: 'Easy Japanese'
-    }
+    } */
   ]
 }
 
