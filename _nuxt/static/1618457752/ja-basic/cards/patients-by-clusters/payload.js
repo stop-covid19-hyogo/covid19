1ea928115819fbf13bbc5cdaded96c53c50d8c5e
ohyama <ohyama@users.noreply.github.com>
2021-04-30 10:04:47 +0000

    deploy: e58c96a2b9b6723045ad821ca74430e0be1ed21a

--- _nuxt/static/1618457752/ja-basic/cards/patients-by-clusters/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/ja-basic/cards/patients-by-clusters", {data:[{}],fetch:[],mutations:void 0});
--- _nuxt/static/1618457752/ko/cards/patients-by-clusters/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/ko/cards/patients-by-clusters", {data:[{}],fetch:[],mutations:void 0});
